1d4f2f169d4479c371fb025e202f6fc0b69a780c
Tara <mtaraalleah@gmail.com>
2026-09-09 15:58:39 +0300

    Added further styling to registration

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,26 +1,27 @@
 const Registration = () => {
-    return (<div className = "registration-form">
-        <form>
-            <div>
+    return (<div className="registration-form">
+        <form className="form-input">
+            <div className="input-form">
                 <label>Name:</label>
                 <input type="text" placeholder="Enter your name"></input>
             </div>
-            <div>
+            <div className="input-form">
                 <label>Email:</label>
                 <input type="text" placeholder="Enter your email"></input>
             </div>
-            <div>
+            <div className="input-form">
                 <label>Password:</label>
                 <input type="password" placeholder="Enter your password"></input>
             </div>
-            <div>
+            <div className="input-form">
                 <label>Confirm password:</label>
                 <input type="password" placeholder="Enter your password"></input>
-            </div>  <div>
+            </div>
+            <div className="input-form">
                 <label>Phone number:</label>
                 <input type="text" placeholder="Enter your phone number"></input>
             </div>
-            <button className = "reg-button">Submit</button>
+            <button className="reg-button">Submit</button>
         </form>
     </div>)
 }
