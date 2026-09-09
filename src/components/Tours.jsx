d56c2ba15c3f56e9fe7ef46cae78d6a86a8b071c
Albaraae <albaraae.17@gmail.com>
2026-09-09 14:41:33 +0300

    refactor: convert Tours to use local state

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,8 +1,10 @@
 import { tours } from "../data";
 import Title from "./Title";
 import Tour from "./Tour";
+import { useState } from "react";
 
 const Tours = () => {
+  const [toursData, setToursData] = useState(tours)
   return (
     <section className="section" id="tours">
       <Title title="featured" subTitle="tours" />
